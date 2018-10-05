b66eabadb1f07b3ad323f2fe1d981c258476ff5f
Raony Guimaraes <raonyguimaraes@gmail.com>
2018-10-05 18:15:02 +0100

    fix concordance

--- src/comparison.py
+++ src/comparison.py
@@ -13,13 +13,23 @@ for line in ancestry_file:
 		row = line.strip().split('\t')
 		ancestry_snps.append(row[0])
 
+		if row[1] == '23':
+			row[1] = 'X'
+		if row[1] == '24':
+			row[1] = 'Y'
+		if row[1] == '26':
+			row[1] = 'MT'
+		
 		index = '{}-{}'.format(row[1], row[2])
 		ancestry_positions.append(index)
 		genotype = '{}{}'.format(row[3], row[4])
 		if len(genotype) != 2:
 			# print('found', row)
 			genotype = '{}{}'.format(row[3],row[3])
-		ancestry_genotypes[row[0]] = ''.join(sorted(genotype))
+		ancestry_genotypes[index] = {}
+		ancestry_genotypes[index]['genotypes'] = ''.join(sorted(genotype))
+		ancestry_genotypes[index]['index'] = index
+
 
 print('ancestry_positions', len(ancestry_positions))
 
@@ -42,22 +52,28 @@ for line in meand23_file:
 		if len(genotype) == 1:
 			# print('found', row)
 			genotype = '{}{}'.format(row[3],row[3])
-		meand23_genotypes[row[0]] = ''.join(sorted(genotype))		
+		meand23_genotypes[index] = {}
+		meand23_genotypes[index]['genotypes'] = ''.join(sorted(genotype))
+		meand23_genotypes[index]['index'] = index
+
 
 common_snps = list(set(ancestry_snps) & set(meand23_snps))
+common_positions = list(set(ancestry_positions) & set(meand23_positions))
 n_commmon=len(common_snps)
 
 print('meand23_positions', len(meand23_positions))
-print('common positions', len(list(set(ancestry_positions) & set(meand23_positions))))
+print('common positions', len(common_positions))
 print('common rsids', n_commmon)
 
 n_concordant = 0
 #concordance
-for snp in common_snps:
+discordant = open('discordant_snps.txt', 'w')
+for snp in common_positions:
 	# print(snp)
-	if ancestry_genotypes[snp] == meand23_genotypes[snp]:
+	if ancestry_genotypes[snp]['genotypes'] == meand23_genotypes[snp]['genotypes']:
 		n_concordant+=1
-	# else:
+	else:
+		discordant.writelines('\t'.join([snp, ancestry_genotypes[snp]['genotypes'], ancestry_genotypes[snp]['index'],  meand23_genotypes[snp]['genotypes'], meand23_genotypes[snp]['index']])+'\n')
 		# print(snp, ancestry_genotypes[snp], meand23_genotypes[snp])
 
 print('n_concordant', n_concordant)
